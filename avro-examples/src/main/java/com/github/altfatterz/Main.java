2018161f560d1f93275668d7b8f3be9d858a59b1
Zoltan Altfatter <altfatterz@gmail.com>
2019-12-27 21:44:14 +0100

    working version

--- avro-examples/src/main/java/com/github/altfatterz/Main.java
+++ avro-examples/src/main/java/com/github/altfatterz/Main.java
@@ -1,22 +1,38 @@
 package com.github.altfatterz;
 
-//import com.github.altfatterz.avro.Account;
-//import com.github.altfatterz.avro.AccountType;
-//import com.github.altfatterz.avro.Customer;
-//import com.github.altfatterz.avro.CustomerStatus;
+import com.github.altfatterz.avro.Account;
+import com.github.altfatterz.avro.AccountType;
+import com.github.altfatterz.avro.Customer;
 
 import java.time.LocalDate;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Customer customer = Customer.newBuilder()
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setBirthDate(LocalDate.of(1960, 10, 10).toEpochDay())
+                .setAccounts(Arrays.asList(
+                        Account.newBuilder()
+                                .setIban("CH93 0076 2011 6238 5295 7")
+                                .setType(AccountType.CHECKING)
+                                .build(),
+                        Account.newBuilder()
+                                .setIban("CH93 0076 2011 6238 5295 8")
+                                .setType(AccountType.SAVING)
+                                .build()
+                ))
+                .setSettings(new HashMap<String, Boolean>() {{
+                    put("e-billing-enabled", true);
+                    put("push-notification-enabled", false);
+                }})
+                .setSignupTimestamp(System.currentTimeMillis())
+                .build();
 
-//        Customer.Builder builder = Customer.newBuilder();
-
-//        builder.setBirthDate(LocalDate.now().toEpochDay());
-//        builder.setCustomerStatus(CustomerStatus.GOLD);
-
-
+        System.out.println(customer);
     }
 }
