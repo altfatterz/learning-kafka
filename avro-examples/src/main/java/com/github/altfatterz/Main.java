44ce624a46f42050e73417a7c5d4c47fbd4a3d68
Zoltan Altfatter <altfatterz@gmail.com>
2019-12-27 19:37:20 +0100

    cleanup

--- avro-examples/src/main/java/com/github/altfatterz/Main.java
+++ avro-examples/src/main/java/com/github/altfatterz/Main.java
@@ -1,9 +1,9 @@
 package com.github.altfatterz;
 
-import com.github.altfatterz.avro.Account;
-import com.github.altfatterz.avro.AccountType;
-import com.github.altfatterz.avro.Customer;
-import com.github.altfatterz.avro.CustomerStatus;
+//import com.github.altfatterz.avro.Account;
+//import com.github.altfatterz.avro.AccountType;
+//import com.github.altfatterz.avro.Customer;
+//import com.github.altfatterz.avro.CustomerStatus;
 
 import java.time.LocalDate;
 
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Customer.Builder builder = Customer.newBuilder();
+//        Customer.Builder builder = Customer.newBuilder();
 
-        builder.setBirthDate(LocalDate.now().toEpochDay());
+//        builder.setBirthDate(LocalDate.now().toEpochDay());
 //        builder.setCustomerStatus(CustomerStatus.GOLD);
 
 
