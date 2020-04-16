041eadbd1b564a6e720b8bf6a14d63db6037ede0
Zoltan Altfatter <altfatterz@gmail.com>
2020-04-16 20:53:46 +0200

    still does not work

--- spring-kafka/wordcount-kafka-streams/src/main/java/com/example/WordCountApp.java
+++ spring-kafka/wordcount-kafka-streams/src/main/java/com/example/WordCountApp.java
@@ -3,11 +3,10 @@ package com.example;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 import java.util.Arrays;
@@ -21,20 +20,19 @@ public class WordCountApp {
     }
 
     @Bean
-    public KTable<String, Long> kStream(StreamsBuilder streamsBuilder) {
+    public KStream<String, String> kStream(StreamsBuilder streamsBuilder) {
 
         KStream<String, String> sentences = streamsBuilder.stream(Config.INPUT_TOPIC);
 
-        KTable<String, Long> wordCounts = sentences
+        sentences
                 .mapValues(sentence -> sentence.toLowerCase())
                 .flatMapValues(sentence -> Arrays.asList(sentence.split("\\W+")))
                 .selectKey((key, word) -> word)
                 .groupByKey()
-                .count();
+                .count()
+                .toStream().to(Config.OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
 
-        sentences.to("word-count-output");
-
-        return wordCounts;
+        return sentences;
     }
 
 }
