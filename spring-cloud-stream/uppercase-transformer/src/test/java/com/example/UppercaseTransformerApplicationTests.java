50fe02f61e01f36c3625f7af70f4f0b6837b00df
Zoltan Altfatter <altfatterz@gmail.com>
2020-05-10 20:57:35 +0200

    add testing example

--- spring-cloud-stream/uppercase-transformer/src/test/java/com/example/UppercaseTransformerApplicationTests.java
+++ spring-cloud-stream/uppercase-transformer/src/test/java/com/example/UppercaseTransformerApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.support.GenericMessage;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// Add example for testing a Message Handler with Function
+// Source: https://cloud.spring.io/spring-cloud-static/spring-cloud-stream/3.0.4.RELEASE/reference/html/spring-cloud-stream.html#_testing
 
 @SpringBootTest
-class UppercaseTransformerApplicationTests {
+@Import(TestChannelBinderConfiguration.class)
+public class UppercaseTransformerApplicationTests {
+
+    @Autowired
+    private InputDestination input;
+
+    @Autowired
+    private OutputDestination output;
 
     @Test
-    void contextLoads() {
+    public void testTransformer() {
+        this.input.send(new GenericMessage<byte[]>("hello".getBytes()));
+        assertThat(output.receive().getPayload()).isEqualTo("HELLO".getBytes());
     }
-
 }
