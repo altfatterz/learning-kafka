38a96c3db79f4b7fd1840751d1ff370076ef8483
Zoltan Altfatter <altfatterz@gmail.com>
2021-03-22 13:47:20 +0100

    add protobuf module and its usage

--- schema-registry/consumer-protobuf/src/main/java/com/example/consumerprotobuf/ConsumerProtobufApplication.java
+++ schema-registry/consumer-protobuf/src/main/java/com/example/consumerprotobuf/ConsumerProtobufApplication.java
@@ -1,8 +1,14 @@
 package com.example.consumerprotobuf;
 
+import com.example.model.Customer.CustomerOuterClass;
+
 public class ConsumerProtobufApplication {
 
     public static void main(String[] args) {
+
+        CustomerOuterClass.Customer customer = CustomerOuterClass.Customer.newBuilder()
+                .setId(1)
+                .build();
 
     }
 
--- schema-registry/producer-protobuf/src/main/java/com/example/ProducerProtobufApplication.java
+++ schema-registry/producer-protobuf/src/main/java/com/example/ProducerProtobufApplication.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import com.example.model.Customer.CustomerOuterClass.Customer;
+
 public class ProducerProtobufApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Customer customer = Customer.newBuilder()
+                .setId(1)
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setEmail("johndoe@gmail.com")
+                .setActive(true)
+                .build();
 
-
-	}
+    }
 
 }
