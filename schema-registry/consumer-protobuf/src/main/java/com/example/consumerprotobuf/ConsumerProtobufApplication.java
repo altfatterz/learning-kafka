758b94c5ed65e2dd2ee7fff95321f489848ce5f3
Zoltan Altfatter <altfatterz@gmail.com>
2021-03-22 12:06:36 +0100

    initial example

---
+++ schema-registry/consumer-protobuf/src/main/java/com/example/consumerprotobuf/ConsumerProtobufApplication.java
@@ -0,0 +1,9 @@
+package com.example.consumerprotobuf;
+
+public class ConsumerProtobufApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ schema-registry/producer-protobuf/src/main/java/com/example/ProducerProtobufApplication.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class ProducerProtobufApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
