ed940d9c094779e8334c899100f4a4de2fbbf8af
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2023-11-09 11:58:40 +0100

    cleanup simple producer and simple consumer

--- spring-kafka/simple-consumer/src/main/java/com/example/simpleconsumer/SimpleConsumerApplication.java
+++ spring-kafka/simple-consumer/src/main/java/com/example/simpleconsumer/SimpleConsumerApplication.java
@@ -1,14 +1,21 @@
 package com.example.simpleconsumer;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +23,28 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
+@RestController
 public class SimpleConsumerApplication {
 
+    private static final String TOPIC = "messages";
+
+    private static final String TOPIC_DLT = "messages.DLT";
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(TOPIC)
+                .partitions(6)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic topicDLT() {
+        return TopicBuilder.name(TOPIC_DLT)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
 
     // The listener containers created for @KafkaListener annotations are not beans in the application context.
     // Instead, they are registered with an infrastructure bean of type KafkaListenerEndpointRegistry
@@ -32,31 +59,12 @@ public class SimpleConsumerApplication {
 
     @Bean
     public CommandLineRunner commandLineRunner() {
-        return args -> registry.getAllListenerContainers().forEach(messageListenerContainer ->
-                logger.info("{}:{}",
-                        messageListenerContainer.getListenerId(),
-                        messageListenerContainer.getGroupId()));
-    }
-
-}
-
-@RestController
-class ConsumerController {
-
-    private KafkaListenerEndpointRegistry registry;
-
-    public ConsumerController(KafkaListenerEndpointRegistry registry) {
-        this.registry = registry;
-    }
-
-    @PostMapping("/containers/{id}")
-    public void start(@PathVariable String id) {
-        registry.getListenerContainer(id).start();
-    }
-
-    @DeleteMapping("/containers/{id}")
-    public void stop(@PathVariable String id) {
-        registry.getListenerContainer(id).stop();
+        logger.info("Review Listener Containers:");
+        return args -> registry.getAllListenerContainers().forEach(messageListenerContainer -> {
+            logger.info("listenerId={},groupId={}",
+                    messageListenerContainer.getListenerId(),
+                    messageListenerContainer.getGroupId());
+        });
     }
 
     @GetMapping("/containers")
@@ -65,32 +73,22 @@ class ConsumerController {
                 .map(c -> c.getListenerId() + ":" + c.isRunning())
                 .collect(Collectors.toSet());
     }
-}
 
-
-@Component
-class Consumer {
-
-    private final Logger logger = LoggerFactory.getLogger(Consumer.class);
-
-    @KafkaListener(id = "messages-container", groupId = "messages-group", topics = Config.TOPIC, concurrency = "3",
-            clientIdPrefix = "${spring.application.name}", autoStartup = "false")
-//    @KafkaListener(id = "messages-container", groupId = "messages-group", topics = Config.TOPIC, concurrency = "3",
-//            clientIdPrefix = "${spring.application.name}")
-//    @KafkaListener(id = "messages-container", groupId = "messages-group", topics = Config.TOPIC, concurrency = "3")
-//    @KafkaListener(id = "messages-container", groupId = "messages-group", topics = Config.TOPIC)
+    @KafkaListener(groupId = "messages-group", topics = TOPIC,
+            clientIdPrefix="${spring.application.name}", concurrency = "3")
     public void consume(String message) {
-        if (message.startsWith("fail")) {
+        if (message.startsWith("poison-pill")) {
             throw new RuntimeException("failed processing message:" + message);
         }
         logger.info("Consumed message -> {}", message);
     }
 
-    @KafkaListener(id = "dlt-messages-container", groupId = "messages-dlt-group", topics = Config.TOPIC_DLT)
+    @KafkaListener(groupId = "messages-dlt-group", topics = TOPIC_DLT,
+            clientIdPrefix = "${spring.application.name}")
     public void dltConsume(String message) {
-        logger.info("Received from {} : {}", Config.TOPIC_DLT, message);
+        logger.info("Received from {} : {}", TOPIC_DLT, message);
     }
 
-
 }
+
 
