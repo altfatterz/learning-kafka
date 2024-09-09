703c97b5358b9ee37e952aba029a441b8644cba0
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-09-09 16:11:05 +0200

    moved the psotgresql-r2dbc to learning-spring project

--- testcontainers/postgresql-r2dbc/src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists customers
-(
-    id serial primary key,
-    firstname varchar(100)  not null,
-    lastname varchar(100) not null
-);
--- testcontainers/postgresql-r2dbc/src/test/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into customers(firstname, lastname) values ('John', 'Doe');
-insert into customers(firstname, lastname) values ('Jane', 'Doe');
-insert into customers(firstname, lastname) values ('John', 'Wick');
